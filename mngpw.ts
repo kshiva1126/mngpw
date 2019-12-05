c4673b49ae511b091f23be45d8185ec395dfa71b
kshiva1126 <42695879+kshiva1126@users.noreply.github.com>
2019-12-06 00:30:58 +0900

    update

--- mngpw.ts
+++ mngpw.ts
@@ -1,8 +1,9 @@
-import * as flags from 'https://deno.land/std@v0.25.0/flags/mod.ts'
-import * as read_file_str from 'https://deno.land/std@v0.25.0/fs/read_file_str.ts'
+import * as flags from 'https://deno.land/std/flags/mod.ts'
+import { readFileStr } from 'https://deno.land/std/fs/read_file_str.ts'
+import { writeJson } from 'https://deno.land/std/fs/write_json.ts'
 
 type FlagsType = {
-  keyword?: string;
+  key?: string;
   u?: string;
   user?: string;
   p?: string;
@@ -17,7 +18,7 @@ type FlagsType = {
 }
 
 type jsonType = {
-  keyword: string;
+  key: string;
   user: string;
   password: string;
 }
@@ -54,9 +55,13 @@ async function checkInput(input: string) {
   }
 }
 
-async function getInput() {
+async function getInput(cannot_find = false) {
   const buf = new Uint8Array(1024);
-  console.log('The file already exists, will you overwrite it? [y/n]');
+  let msg = 'The file already exists, will you overwrite it? [y/n]';
+  if (cannot_find) {
+    msg = '.mngpw file cannot find, will you create it? [y/n]';
+  }
+  console.log(msg);
   const n = await Deno.stdin.read(buf); 
   if (n == Deno.EOF) {
     console.log("Standard input closed")
@@ -67,8 +72,8 @@ async function getInput() {
 }
 
 async function initFile() {
-  const dotfile = `${Deno.homeDir}/.mngpw`;
-  read_file_str.readFileStr(dotfile)
+  const dotfile = `${Deno.homeDir()}/.mngpw`;
+  readFileStr(dotfile)
   .then(() => {
     getInput();
   })
@@ -77,33 +82,44 @@ async function initFile() {
   })
 }
 
-function register(keyword: string, user: string, password: string) {
-  console.log(keyword, user, password);
+async function register(key: string, user: string, password: string) {
+  const dotfile = `${Deno.homeDir()}/.mngpw`;
+  const json = await readFileStr(dotfile, {encoding: 'utf-8'})
+  .catch(() => {
+    getInput(true);
+  })
+  let arr: jsonType[] = typeof json == 'string' ? JSON.parse(json) : new Array();
   // JSONに登録する
+  const obj: jsonType = {key, user, password};
+  arr.push(obj);
+  const uniqArr = arr.filter((x, i, self) => (
+    self.findIndex(y => y.key === x.key) === i
+  ));
+  writeJson(dotfile, uniqArr);
 }
 
-function show(keyword: string) {
-  console.log(keyword);
-  // JSONからkeywordに合致したuser:passwordを出力する
+function show(key: string) {
+  console.log(key);
+  // JSONからkeyに合致したuser:passwordを出力する
 }
 
 async function main () {
-  const { keyword, u, user, p, password, s, store, d, display, init } = flags.parse(Deno.args) as FlagsType;
+  const { key, u, user, p, password, s, store, d, display, init } = flags.parse(Deno.args) as FlagsType;
 
   if (s || store) {
-    if ((keyword) && (u || user) && (p || password)) {
+    if ((key) && (u || user) && (p || password)) {
       const user_val: string = u ? u : user;
       const password_val: string = p ? p : password;
-      register(keyword, user_val, password_val);
+      register(key, user_val, password_val);
       return;
     }
-    console.log('Please choose -s option with --keyword, -u and -p options');
+    console.log('Please choose -s option with --key, -u and -p options');
     return;
   }
 
   if (d || display) {
-    if (keyword) {
-      show(keyword);
+    if (key) {
+      show(key);
       return;
     }
     console.log('Please choose -d option with -t option.');
